f4310dc917218b8f6adde950e3f4a52491552bae
farrukh-ahm <farrukh.adnan@yahoo.co.in>
2023-08-16 23:11:20 +0000

    Add email input field and add validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,23 @@
-function myFunction(x) {
-    if (x.matches) { // If media query matches
-      document.body.style.backgroundColor = "yellow";
-    } else {
-      document.body.style.backgroundColor = "pink";
-    }
+const submit = document.querySelector(".submit")
+const emailListener = document.querySelector(".email")
+
+emailListener.addEventListener("input", (e)=>{
+  userInput = e.target.value;
+  if(userInput.trim().includes("@")){
+    submit.classList.add("valid")
+    emailListener.classList.remove("invalid")
   }
-  
-  var x = window.matchMedia("(max-width: 700px)")
-//   myFunction(x) // Call listener function at run time
-  x.addListener(myFunction) // Attach listener function on state changes 
+  else{
+    submit.classList.remove("valid")
+  }
+})
+
+
+submit.addEventListener("click", ()=>{
+  if(emailListener.value.trim().includes("@")){
+    emailListener.classList.remove("invalid")
+  }
+  else{
+    emailListener.classList.add("invalid")
+  }
+})
