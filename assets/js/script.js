09840390e66e352ffbe8024933da66a071d8b2cf
farrukh-ahm <farrukh.adnan@yahoo.co.in>
2023-08-17 00:37:15 +0000

    Add confirmation modal box

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
 const submit = document.querySelector(".submit")
 const emailListener = document.querySelector(".email")
+const dialogBox = document.querySelector(".modal")
+const modalClose = document.querySelector(".modal-close")
+const para = document.querySelector(".paragraph")
 
 emailListener.addEventListener("input", (e)=>{
   userInput = e.target.value;
@@ -16,8 +19,15 @@ emailListener.addEventListener("input", (e)=>{
 submit.addEventListener("click", ()=>{
   if(emailListener.value.trim().includes("@")){
     emailListener.classList.remove("invalid")
+    para.innerText = `A confirmation email has been sent to ${emailListener.value}. 
+    Please open it and click the button inside to confirm your subscription.`
+    dialogBox.showModal()
   }
   else{
     emailListener.classList.add("invalid")
   }
+})
+
+modalClose.addEventListener("click", ()=>{
+  dialogBox.close()
 })
